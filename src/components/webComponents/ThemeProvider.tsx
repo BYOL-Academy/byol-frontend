22bde21a06aa63d58d3b31cd1fb1cbaa3d004e7d
S4NFS <zyzzaestheticsgods@gmail.com>
2025-05-26 15:08:20 +0530

    Refactor: Deleted Byol landing components

--- src/components/webComponents/ThemeProvider.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
-
-export function ByolThemeProvider({ children, ...props }: Readonly<ThemeProviderProps>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
